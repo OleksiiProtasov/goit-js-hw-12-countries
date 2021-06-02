bb35b4321bc53dfabc959a9e5753381ffd4b0b72
Oleksii Protasov <oleksi.protasov@gmail.com>
2021-06-02 16:33:52 +0300

    +

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -2594,7 +2594,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4663" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6362" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
