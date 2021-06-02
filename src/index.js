9e672935afa2db89cd35f34512f57403461f6078
Oleksii Protasov <oleksi.protasov@gmail.com>
2021-06-02 16:18:21 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import './sass/main.scss';
+import './styles.css';
+import { debounce } from "debounce";
+import fetchCountries from './js/fetchCountries.js'
+import updateMarkup from './js/updateMarkup.js'
+import {  defaultModules,error} from '@pnotify/core';
+import * as PNotifyDesktop from '@pnotify/desktop/dist/PNotifyDesktop';
+import "@pnotify/core/dist/PNotify.css" 
+import "@pnotify/desktop/dist/PNotifyDesktop" ;
+import '@pnotify/core/dist/BrightTheme.css';
+
+const refs= {
+  searchForm: document.querySelector('input'),
+  countryContainer : document.querySelector('.countryContainer')
+  }
+  
+const onInputChange = (e) => {
+  e.preventDefault()
+
+ refs.countryContainer.innerHTML = ''
+
+ const inputValue = e.target.value;
+
+ if(inputValue){
+fetchCountries(inputValue.trim()).then(data=> updateMarkup(data))
+.catch(error=>console.log('error'))}
+}
+
+refs.searchForm.addEventListener('input', debounce(onInputChange, 500))
+
+
